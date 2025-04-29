4cb192dfaf080d6e56a4dc6d6278a5e70eadf970
Filipe Moraes <filipemoraes@Mac.lan>
2025-04-29 22:17:40 +0100

    fix(slug): prevents the maximum call size exceeded in the relationships between contents

--- server/src/utils/populateQueryFromContentType.ts
+++ server/src/utils/populateQueryFromContentType.ts
@@ -1,12 +1,12 @@
 import type { Core } from '@strapi/strapi';
 
-function handleAttributes(attributes) {
+function handleAttributes(attributes, level) {
   const query: any = {};
   Object.keys(attributes).forEach((key) => {
     switch (attributes[key]['type']) {
       case 'component':
         const componentData = strapi.components[attributes[key]['component']];
-        query[key] = { populate: handleAttributes(componentData.attributes) };
+        query[key] = { populate: handleAttributes(componentData.attributes, level) };
         break;
       case 'media':
         query[key] = { populate: '*' };
@@ -16,14 +16,24 @@ function handleAttributes(attributes) {
         query[key] =
           key === 'localizations'
             ? { populate: '*' }
-            : { populate: populateQueryFromContentType(strapi, attributes[key]['target']) };
+            : level === 3
+              ? { populate: '*' }
+              : {
+                  populate: populateQueryFromContentType(
+                    strapi,
+                    attributes[key]['target'],
+                    level + 1
+                  ),
+                };
         break;
       case 'dynamiczone':
         const components = attributes[key]['components'];
         query[key] = { on: {} };
         components.forEach((component) => {
           const componentData = strapi.components[component];
-          query[key]['on'][component] = { populate: handleAttributes(componentData.attributes) };
+          query[key]['on'][component] = {
+            populate: handleAttributes(componentData.attributes, level),
+          };
         });
         break;
       default:
@@ -33,8 +43,8 @@ function handleAttributes(attributes) {
   return Object.keys(query).length === 0 ? '*' : query;
 }
 
-export function populateQueryFromContentType(strapi: Core.Strapi, contentType: string) {
+export function populateQueryFromContentType(strapi: Core.Strapi, contentType: string, level = 1) {
   const attributes = strapi.contentTypes[contentType]['attributes'];
-  const query = handleAttributes(attributes);
+  const query = handleAttributes(attributes, level);
   return query;
 }
