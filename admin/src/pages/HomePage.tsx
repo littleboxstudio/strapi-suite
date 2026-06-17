774930380bbaa12c01eef61c5d3225abc8814b2f
Filipe Moraes <filipemoraes87@gmail.com>
2026-06-17 03:19:56 +0100

    chore: bump version to 1.0.49

--- admin/src/pages/HomePage.tsx
+++ admin/src/pages/HomePage.tsx
@@ -31,7 +31,7 @@ const HomePage = () => {
             window.open('https://www.npmjs.com/package/@littlebox/strapi-suite', '_blank')
           }
         >
-          v1.0.48
+          v1.0.49
         </Button>
         <Button
           size="S"
